8e527c51392f5b1fc89361712fafac2bdb3909a5
Simon Balanoff <sjbalanoff@gmail.com>
2025-11-07 10:12:34 -0700

    Initial commit

---
+++ ResidentFeedback-App/ResidentFeedback_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResidentFeedback_AppApp.swift
+//  ResidentFeedback-App
+//
+//  Created by Simon Balanoff on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ResidentFeedback_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
